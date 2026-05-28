e5c707489548002e932df7a20d2bd2f617799f5a
panickus <felipedg@outlook.es>
2026-05-28 20:09:53 +0200

    revert: access private not supported by vercel blob plugin

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -43,7 +43,6 @@ export default buildConfig({
         media: true,
       },
       token: process.env.BLOB_READ_WRITE_TOKEN || "",
-      access: "private",
     }),
   ],
   cors: ["https://kentodevlab.com"],
