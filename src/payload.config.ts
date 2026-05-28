ea8ac7de922c4c8455959c8cf3f8588a30aa695b
panickus <felipedg@outlook.es>
2026-05-28 20:06:20 +0200

    fix: set Vercel Blob access to private to match store config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -43,6 +43,7 @@ export default buildConfig({
         media: true,
       },
       token: process.env.BLOB_READ_WRITE_TOKEN || "",
+      access: "private",
     }),
   ],
   cors: ["https://kentodevlab.com"],
